3d5e6457cd894d04f6bef67b5b3d8562aee5b162
wulahae <47910605+wulahae@users.noreply.github.com>
2025-04-04 18:39:31 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import ParcelOptimizer from "./ParcelOptimizer";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-black">
+      <ParcelOptimizer />
+    </div>
+  );
+}
